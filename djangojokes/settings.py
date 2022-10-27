b78845f3fe1bde4f5212b2c1c591af169167bc0a
samoore333 <samoore333@gmail.com>
2022-10-27 13:45:41 -0700

    Custom User Model setup

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'common.apps.CommonConfig',
     'jokes.apps.JokesConfig',
     'pages.apps.PagesConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -95,6 +96,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+# AUTHENTICATION SETTINGS
+AUTH_USER_MODEL = 'users.CustomUser'
 
 
 # Internationalization
