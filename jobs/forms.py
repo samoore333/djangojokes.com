d425c8c169072bdfdd9f117ea389dd83a1e0165a
samoore333 <samoore333@gmail.com>
2022-11-01 17:15:44 -0700

    Job Application form update

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,28 +1,56 @@
+from datetime import datetime
 from django import forms
 
 TYPES = (
     (None, '--Please choose--'),
-    ('full_time', 'Full-time'),
-    ('part_time', 'Part-time'),
-    ('contract_work', 'Contract work')
+    ('ft', 'Full-time'),
+    ('pt', 'Part-time'),
+    ('contract', 'Contract work')
 )
 
 DAYS = (
-    ('mon', 'MON'),
-    ('tue', 'TUE'),
-    ('wed', 'WED'),
-    ('thu', 'THU'),
-    ('fri', 'FRI')
+    (1, 'MON'),
+    (2, 'TUE'),
+    (3, 'WED'),
+    (4, 'THU'),
+    (5, 'FRI')
 )
 
+YEARS = range(datetime.now().year, datetime.now().year+2)
+
 class JobApplicationForm(forms.Form):
-    first_name = forms.CharField()
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'autofocus': True})
+    )
     last_name = forms.CharField()
     email = forms.EmailField()
-    website = forms.URLField(required=False)
+    website = forms.URLField(
+        required=False, 
+        widget=forms.URLInput(
+            attrs={'placeholder':'https://www.example.com','size': 50}
+        )
+    )
     employment_type = forms.ChoiceField(choices=TYPES)
-    start_date = forms.DateField(help_text='The earliest date you can start working.')
-    available_days = forms.MultipleChoiceField(choices=DAYS, help_text='Select all days that you can work.')
-    desired_wage = forms.DecimalField()
-    cover_letter = forms.CharField()
-    certify = forms.BooleanField(label='I certify that the information I have provided is true.', required=True)
+    start_date = forms.DateField(
+        help_text='The earliest date you can start working.',
+        widget=forms.SelectDateWidget(
+            years=YEARS,
+        )
+    )
+    available_days = forms.MultipleChoiceField(
+        choices=DAYS,
+        help_text='Select all days that you can work.',
+        widget=forms.CheckboxSelectMultiple(
+            attrs={'checked':True}
+        )
+    )
+    desired_wage = forms.DecimalField(
+        widget=forms.NumberInput(
+            attrs={'min': '10.00','max': '100.00','step': '.25'})
+    )
+    cover_letter = forms.CharField(
+        widget=forms.Textarea(attrs={'cols': '75', 'rows': '5'})
+    )
+    certify = forms.BooleanField(
+        label='I certify that the information I have provided is true.'
+        )
