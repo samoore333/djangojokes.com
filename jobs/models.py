2ee48b53a26ec48186ec2973eae351af6c68f231
samoore333 <samoore333@gmail.com>
2022-11-10 13:06:54 -0800

    File Type setup

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,4 @@
+import filetype
 from datetime import datetime
 
 from django.core.exceptions import ValidationError
@@ -9,6 +10,11 @@ def validate_future_date(value):
         raise ValidationError(
             message=f'{value} is in the past.', code='past_date'
         )
+
+def validate_pdf(value):
+    kind = filetype.guess(value)
+    if not kind or kind.mime != 'application/pdf':
+        raise ValidationError("That’s not a PDF file.")
 
 class Job(models.Model):
     title = models.CharField(max_length=200)
@@ -40,7 +46,8 @@ class Applicant(models.Model):
     desired_hourly_wage = models.DecimalField(max_digits=5, decimal_places=2)
     cover_letter = models.TextField()
     resume = models.FileField(
-        upload_to='private/resumes', blank=True, help_text='PDFs only'
+        upload_to='private/resumes', blank=True, help_text='PDFs only',
+        validators=[validate_pdf]
     )
     confirmation = models.BooleanField()
     job = models.ForeignKey(Job, on_delete=models.CASCADE)
