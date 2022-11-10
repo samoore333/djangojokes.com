7b1981d0554c23019f4a7f070aea88b80e4bb6f8
samoore333 <samoore333@gmail.com>
2022-11-10 13:41:11 -0800

    File private setup

--- djangojokes/settings.py
+++ djangojokes/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'private_storage',
 
     # Local apps
     'common.apps.CommonConfig',
@@ -166,6 +167,10 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# private-storage settings
+PRIVATE_STORAGE_ROOT = MEDIA_ROOT / 'private/'
+PRIVATE_STORAGE_AUTH_FUNCTION = 'private_storage.permissions.allow_staff'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- jobs/models.py
+++ jobs/models.py
@@ -1,4 +1,5 @@
 import filetype
+from private_storage.fields import PrivateFileField
 from datetime import datetime
 
 from django.core.exceptions import ValidationError
@@ -45,8 +46,8 @@ class Applicant(models.Model):
     available_days = models.CharField(max_length=20)
     desired_hourly_wage = models.DecimalField(max_digits=5, decimal_places=2)
     cover_letter = models.TextField()
-    resume = models.FileField(
-        upload_to='private/resumes', blank=True, help_text='PDFs only',
+    resume = PrivateFileField(
+        upload_to='resumes', blank=True, help_text='PDFs only',
         validators=[validate_pdf]
     )
     confirmation = models.BooleanField()
